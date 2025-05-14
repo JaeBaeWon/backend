4b4c727ddf853a216723eaa61dd6336b96fc088d
Jeongho <fgh2585@gmail.com>
2025-05-14 15:09:53 +0900

    feat: test branch

--- src/main/java/org/example/backend/domain/payment/controller/PaymentController.java
+++ src/main/java/org/example/backend/domain/payment/controller/PaymentController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/payment")
 public class PaymentController {
 
