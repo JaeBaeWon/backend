9db3f5690187a2a61bc50acbb92f4827bf2e0753
nanahj <dlguswjd221@naver.com>
2025-05-22 09:53:35 +0900

    feat: import api key 변수 설정

--- src/main/java/org/example/backend/global/config/IamportConfig.java
+++ src/main/java/org/example/backend/global/config/IamportConfig.java
@@ -2,15 +2,21 @@ package org.example.backend.global.config;
 
 
 import com.siot.IamportRestClient.IamportClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class IamportConfig {
 
+    @Value("${iamport.api-key}")
+    private String apiKey;
+
+    @Value("${iamport.api-secret}")
+    private String apiSecret;
+
     @Bean
     public IamportClient iamportClient() {
-        return new IamportClient("8188752611441763",
-                "aKEIP08BMJcaHltoTZKK5YluO56KbvVPLKNGXTzhDZr8iq8avAeAn7qi1KN5H0t8QATio0yXLQtg9ITQ");
+        return new IamportClient(apiKey, apiSecret);
     }
 }
