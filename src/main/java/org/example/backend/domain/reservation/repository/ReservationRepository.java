3e7187ea9077c03ee9fdaed43ea29602edad98b5
nanahj <dlguswjd221@naver.com>
2025-05-28 19:59:49 +0900

    feat: push 시험

--- src/main/java/org/example/backend/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/org/example/backend/domain/reservation/repository/ReservationRepository.java
@@ -2,7 +2,6 @@ package org.example.backend.domain.reservation.repository;
 
 import org.example.backend.domain.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
