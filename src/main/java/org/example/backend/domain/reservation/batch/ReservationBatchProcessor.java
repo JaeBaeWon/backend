c59b34adf3c2140c4ad7a9379cc6a4721b107749
Jeongho <fgh2585@gmail.com>
2025-06-04 15:34:13 +0900

    feat: redis batch log 추가

--- src/main/java/org/example/backend/domain/reservation/batch/ReservationBatchProcessor.java
+++ src/main/java/org/example/backend/domain/reservation/batch/ReservationBatchProcessor.java
@@ -1,11 +1,14 @@
 package org.example.backend.domain.reservation.batch;
 
+
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RQueue;
 import org.redisson.api.RedissonClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ReservationBatchProcessor {
@@ -17,9 +20,27 @@ public class ReservationBatchProcessor {
     public void processReservationQueue() {
         RQueue<String> queue = redissonClient.getQueue("reservation:queue");
 
+        log.info("🌀 예약 배치 시작");
+
         String json;
+        int count = 0;
+
         while ((json = queue.poll()) != null) {
-            reservationBatchService.process(json);
+            log.info("📦 처리할 메시지: {}", json);
+            try {
+                reservationBatchService.process(json);
+                count++;
+            } catch (Exception e) {
+                log.error("❌ 메시지 처리 실패: {}", e.getMessage(), e);
+            }
         }
+
+        if (count == 0) {
+            log.info("📭 처리할 메시지가 없습니다.");
+        } else {
+            log.info("✅ 총 {}건의 메시지 처리 완료", count);
+        }
+
+        log.info("🏁 예약 배치 종료");
     }
 }
