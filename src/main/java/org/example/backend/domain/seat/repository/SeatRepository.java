e06d945f3a38c7d214bd3b5bb4746347cc512dc0
Jeongho <fgh2585@gmail.com>
2025-06-04 19:05:51 +0900

    fix: 예매 메일 로직 수정

--- src/main/java/org/example/backend/domain/seat/repository/SeatRepository.java
+++ src/main/java/org/example/backend/domain/seat/repository/SeatRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
     List<Seat> findAllByPerformance(Performance perform);
+
+    Seat findByPerformance(Performance performance);
 }
