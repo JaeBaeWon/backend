7e945386b75946f484c0d75c17e03d55353c2784
kcklkb <kchaklkb@naver.com>
2025-06-05 17:42:55 +0900

    fix: rest api gateway 부분 수정

--- src/main/java/org/example/backend/global/config/RestApiGatewayConfig.java
+++ src/main/java/org/example/backend/global/config/RestApiGatewayConfig.java
@@ -1,18 +1,15 @@
 package org.example.backend.global.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@Configuration
-@ConfigurationProperties(prefix = "rest-api-gateway")
+@Component
 public class RestApiGatewayConfig {
+
+    @Value("${rest-api-gateway.url}")
     private String url;
 
     public String getUrl() {
         return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 }
--- src/main/java/org/example/backend/global/restapigateway/RestApiGatewayController.java
+++ src/main/java/org/example/backend/global/restapigateway/RestApiGatewayController.java
@@ -3,19 +3,20 @@ package org.example.backend.global.restapigateway;
 import org.example.backend.global.config.RestApiGatewayConfig;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Map;
 
 @RestController
 public class RestApiGatewayController {
-    private final RestApiGatewayConfig properties;
 
-    public RestApiGatewayController(RestApiGatewayConfig properties) {
-        this.properties = properties;
+    private final RestApiGatewayConfig config;
+
+    public RestApiGatewayController(RestApiGatewayConfig config) {
+        this.config = config;
     }
 
     @GetMapping("/config")
-    public Map<String, String> getConfig() {
-        return Map.of("restApiGatewayUrl", properties.getUrl());
+    public Map<String, String> getGatewayUrl() {
+        return Map.of("restApiGatewayUrl", config.getUrl());
     }
 }
-
